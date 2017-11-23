042e62bea73c2d095031298e9192b28af554c333
vinod10 <vinodkumar@kwiqpick.com>
2017-11-23 15:02:06 +0530

    added components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -8,13 +8,20 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipesListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A test', 'desc', 'http://vegecravings.com/wp-content/uploads/2017/04/kadai-paneer-dry-recipe-step-by-step-instructions.jpg')
+    new Recipe('A test', 'desc', 'http://vegecravings.com/wp-content/uploads/2017/04/kadai-paneer-dry-recipe-step-by-step-instructions.jpg'),
+    new Recipe('test 2', 'desc 2', 'http://vegecravings.com/wp-content/uploads/2017/04/kadai-paneer-dry-recipe-step-by-step-instructions.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 }
